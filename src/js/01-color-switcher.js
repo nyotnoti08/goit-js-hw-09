ab6076de92ea3f754507ed93d8adf74d114922c0
nyotnoti08 <ynoalcala08@gmail.com>
2023-12-18 07:21:31 +0800

    Add Code for HW 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  const randColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+      return randColor;
+    }
 
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+
+    let intervalId = null;
+
+    // start changing background colors per 1 second
+    function startColorChange() {
+      startButton.disabled = true;
+      stopButton.disabled = false;
+
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    }
+
+    // Stop changing colors function
+    function stopColorChange() {
+      startButton.disabled = false;
+      stopButton.disabled = true;
+
+      clearInterval(intervalId);
+    }
+
+    // buttons for evet function
+    startButton.addEventListener('click', startColorChange);
+    stopButton.addEventListener('click', stopColorChange);
